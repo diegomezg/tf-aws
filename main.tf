59661afcd7305bb240d7033b51966de8e0cbb6f3
Diego Gomez <diego.gomez@digitalonus.com>
2021-05-10 14:01:43 -0500

    tf aws vpc code

--- main.tf
+++ main.tf
@@ -13,47 +13,45 @@
 
 #aws code
 
-# Create a VPC
-resource "aws_security_group" "dg-tf-sg" {
-  name        = "dg-tf-sg"
-  description = "Tf security group demo"
-  #vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "SSH ALLOWED"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "ssh"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "TCP ALLOWED"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "dg-tf-sg"
-  }
-}
-
-
-r#aws vpcesource "aws_vpc" "main" {
+resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-
   tags = {
-    Name = "main"
+    Name = "dg-vpc"
   }
 }
+
+# Create a Security group
+# resource "aws_security_group" "dg-tf-sg" {
+#   name        = "dg-tf-sg"
+#   description = "Tf security group demo"
+#   vpc_id      = aws_vpc.main.id
+
+#   ingress {
+#     description = "SSH ALLOWED"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     description = "TCP ALLOWED"
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+
+#   tags = {
+#     Name = "dg-tf-sg"
+#   }
+# }
